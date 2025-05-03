0779239aa2b5f6af32e8c7d9e9af92ed32aceda7
Ahmed Mohamed Abdelalim <58574407+ahmmedabdelalim@users.noreply.github.com>
2025-05-03 14:25:12 +0300

    Delete 04-spring-boot-rest-crud/05-spring-boot-rest-crud-students-exception-handling directory

--- 04-spring-boot-rest-crud/05-spring-boot-rest-crud-students-exception-handling/src/main/java/com/lemo/demo/Exception/StudentNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.lemo.demo.Exception;
-
-public class StudentNotFoundException extends RuntimeException {
-
-    public StudentNotFoundException(String message)
-    {
-        super(message);
-    }
-}
